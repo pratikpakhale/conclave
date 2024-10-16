9b0679dff46f93343742a527576c11c99fe6eb13
Pratik Pakhale <pratikpakhale20@gmail.com>
2024-10-17 03:28:50 +0530

    fix: merge

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,28 @@
 // 'use client';
 
 // import About from '@/components/Home/About';
-import Footer from "@/components/Footer";
+import Footer from '@/components/Footer';
 // import HeroSection from "@/components/Home/HeroSection";
-import Navbar from "@/components/Navbar";
+import Navbar from '@/components/Navbar';
 
-import SmoothScroll from "@/components/SmoothScroll";
-import TimeLine from "@/components/Home/TimeLine";
-import OverTheYears from "@/components/Home/OverTheYears";
+import SmoothScroll from '@/components/SmoothScroll';
+import TimeLine from '@/components/Home/TimeLine';
+import OverTheYears from '@/components/Home/OverTheYears';
 // import Testimonials from "@/components/Testimonials/Testimonials";
-import { LandingSection } from "@/components/Home/LandingSection";
-import { TestimonialMain } from "@/components/Home/Testimonials/TestimonialMain";
+import { LandingSection } from '@/components/Home/LandingSection';
+import { TestimonialMain } from '@/components/Home/Testimonials/TestimonialMain';
+import TestimonialsPage from '@/components/Testimonials';
 
 export default function Home() {
   return (
-    <div className="w-full flex flex-col font-urbanist">
+    <div className='w-full flex flex-col font-urbanist'>
       <SmoothScroll>
         <Navbar />
         <LandingSection />
         {/* <HeroSection /> */}
         {/* <About /> */}
-        {/* <TestimonialsPage /> */}
-        <TestimonialMain />
+        <TestimonialsPage />
+        {/* <TestimonialMain /> */}
         <TimeLine />
         <OverTheYears />
       </SmoothScroll>

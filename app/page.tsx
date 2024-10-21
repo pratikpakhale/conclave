7a5c5fdda0fc516bc85c148bcb256a9b323c9a8c
Pratik Pakhale <pratikpakhale20@gmail.com>
2024-10-21 19:00:41 +0530

    tmp

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,28 @@
 // 'use client';
 
 // import About from '@/components/Home/About';
-import Footer from "@/components/Footer";
+import Footer from '@/components/Footer';
 // import HeroSection from "@/components/Home/HeroSection";
-import Navbar from "@/components/Navbar";
+import Navbar from '@/components/Navbar';
 
 // import SmoothScroll from "@/components/SmoothScroll";
-import TimeLine from "@/components/Home/TimeLine";
-import OverTheYears from "@/components/Home/OverTheYears";
+import TimeLine from '@/components/Home/TimeLine';
+import OverTheYears from '@/components/Home/OverTheYears';
 // import Testimonials from "@/components/Testimonials/Testimonials";
-import { LandingSection } from "@/components/Home/LandingSection";
+import { LandingSection } from '@/components/Home/LandingSection';
 // import { TestimonialMain } from "@/components/Home/Testimonials/TestimonialMain";
-import TestimonialsPage from "@/components/Testimonials";
+import TestimonialsPage from '@/components/Testimonials';
 
 export default function Home() {
   return (
-    <div className="w-full flex flex-col font-grotesk">
+    <div className='w-full flex flex-col font-grotesk'>
       <Navbar />
       <LandingSection />
       {/* <HeroSection /> */}
       {/* <About /> */}
-      <TestimonialsPage />
+      {/* <TestimonialsPage /> */}
       {/* <TestimonialMain /> */}
-      <TimeLine />
+      {/* <TimeLine /> */}
       <OverTheYears />
       <Footer />
     </div>

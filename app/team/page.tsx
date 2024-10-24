f1d20d0c95861e40b6b96f147edf57e5aa067f58
C Nikhil Karthik <nikhilkarthik241103@gmail.com>
2024-10-24 23:47:04 +0530

    fix: bugs

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import React from "react";
-import Footer from "@/components/Footer";
+// import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import {
+  EMCEETeam,
   Invitation_Team_Members,
+  VideoBytes,
   foodCommitteeMembers,
+  sponsorshipteam,
   studentVolunteerTeam,
   transportationHospitalityTeam,
 } from "@/data/team";
@@ -12,13 +15,14 @@ import { Registration_Team_Members } from "@/data/team";
 import { Program_Team_Members } from "@/data/team";
 import { stageCommitteeMembers } from "@/data/team";
 import Accordion from "@/components/team/Accordion";
+import Footer from "@/components/Footer";
 
 export default function Page() {
   return (
     <main className="">
       <Navbar />
-      <div className="bg-text-col min-h-screen h-full w-full relative pb-40">
-        <div className="w-full relative text-color1 min-h-screen h-full pt-40 items-center bg-text-col flex flex-col">
+      <div className="bg-text-col min-h-screen h-full w-full relative">
+        <div className="w-full relative text-color1 min-h-screen h-full pt-40  pb-40 items-center bg-text-col flex flex-col">
           <p className="text-5xl text-center font-bold">Team Members</p>
           <Accordion
             Heading="Invitation, Media and Management Committee "
@@ -42,11 +46,18 @@ export default function Page() {
             List={transportationHospitalityTeam}
           />
           <Accordion
+            Heading="Sponsorship and Outreach Committee"
+            List={sponsorshipteam}
+          />
+          <Accordion
             Heading="Student Coordination and Volunteers Committee "
             List={studentVolunteerTeam}
           />
+          <Accordion Heading="EMCEE Team" List={EMCEETeam} />
+          <Accordion Heading="Video Bytes Committee" List={VideoBytes} />
         </div>
         {/* <div className="bg-text-col h-40 w-full"></div> */}
+        <Footer />
       </div>
     </main>
   );

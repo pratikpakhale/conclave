a2ba8b528821dea3c8f35fee444ad6f1c2ba0855
Pratik Pakhale <pratikpakhale20@gmail.com>
2024-11-01 01:29:08 +0530

    fix: duplicate testimonials

--- components/Home/Testimonials/TestimonialMain.tsx
+++ components/Home/Testimonials/TestimonialMain.tsx
@@ -1,10 +1,10 @@
-"use client";
-import { useState, useEffect } from "react";
-import Marquee from "@/components/ui/marquee";
-import { getTestimonials } from "@/app/actions/testimonials";
-import { ReviewCard, ReviewDialog } from "./DialogCard";
-import { Review } from "@/types/Home";
-import ScrollReveal from "@/components/ScrollReveal";
+'use client';
+import { useState, useEffect } from 'react';
+import Marquee from '@/components/ui/marquee';
+import { getTestimonials } from '@/app/actions/testimonials';
+import { ReviewCard, ReviewDialog } from './DialogCard';
+import { Review } from '@/types/Home';
+import ScrollReveal from '@/components/ScrollReveal';
 
 export function TestimonialMain() {
   const [reviews, setReviews] = useState<Review[]>([]);
@@ -21,15 +21,20 @@ export function TestimonialMain() {
     fetchTestimonials();
   }, []);
 
-  const firstRow = reviews.slice(0, reviews.length / 2);
-  const secondRow = reviews.slice(reviews.length / 2);
+  const quarterLength = Math.floor(reviews.length / 4);
+  const firstRow = reviews.slice(0, quarterLength);
+  const secondRow = reviews.slice(quarterLength, quarterLength * 2);
+  const thirdRow = reviews.slice(quarterLength * 2, quarterLength * 3);
+  const fourthRow = reviews.slice(quarterLength * 3, reviews.length);
+
+  console.log(firstRow, secondRow, thirdRow, fourthRow);
 
   const renderMarquee = (rowData: Review[], reverse = false) => (
     <Marquee
       vertical
       reverse={reverse}
       pauseOnHover
-      className="[--duration:50s]"
+      className='[--duration:50s]'
     >
       {rowData.map((review, index) => (
         <ReviewCard
@@ -42,7 +47,7 @@ export function TestimonialMain() {
   );
 
   const mobileRenderMarquee = (rowData: Review[], reverse = false) => (
-    <Marquee vertical reverse={reverse} className="[--duration:50s]">
+    <Marquee vertical reverse={reverse} className='[--duration:50s]'>
       {rowData.map((review, index) => (
         <ReviewCard
           key={index}
@@ -54,39 +59,39 @@ export function TestimonialMain() {
   );
 
   return (
-    <section className="w-full px-2 sm:px-4 md:px-8px lg:px-24px xl:px-48px flex flex-col py-20 text-text-col bg-color1">
-      <div className="bg-color1 px-4 py-20 w-full text-center leading-[1.1] tracking-tighter flex items-center gap-2 flex-col">
+    <section className='w-full px-2 sm:px-4 md:px-8px lg:px-24px xl:px-48px flex flex-col py-20 text-text-col bg-color1'>
+      <div className='bg-color1 px-4 py-20 w-full text-center leading-[1.1] tracking-tighter flex items-center gap-2 flex-col'>
         <ScrollReveal>
-          <div className="text-48px font-bold">
-            Explore inspiring{" "}
+          <div className='text-48px font-bold'>
+            Explore inspiring{' '}
             <span className="bg-clip-text text-transparent bg-[url('https://www.apple.com/careers/images/fy21/apple_jobs_gradient-final_Apple_Jobs_Gradients_Warm/desktop@2x.png')] [-webkit-text-fill-color:transparent] [-webkit-box-decoration-break:clone] [background-size:100%_100%]">
-              journeys and achievements{" "}
+              journeys and achievements{' '}
             </span>
           </div>
         </ScrollReveal>
         <ScrollReveal>
-          <div className="text-48px font-bold ">
+          <div className='text-48px font-bold '>
             from our thriving alumni community.
           </div>
         </ScrollReveal>
       </div>
 
-      <div className="relative flex h-[80vh] w-full items-center justify-center overflow-hidden bg-color1 md:shadow-xl">
-        <div className="md:hidden basis-full md:basis-1/2 lg:basis-1/3 xl:basis-1/4">
+      <div className='relative flex h-[80vh] w-full items-center justify-center overflow-hidden bg-color1 md:shadow-xl'>
+        <div className='md:hidden basis-full md:basis-1/2 lg:basis-1/3 xl:basis-1/4'>
           {mobileRenderMarquee(reviews)}
         </div>
-        <div className="max-md:hidden basis-full md:basis-1/2 lg:basis-1/3 xl:basis-1/4">
+        <div className='max-md:hidden basis-full md:basis-1/2 lg:basis-1/3 xl:basis-1/4'>
           {renderMarquee(firstRow)}
         </div>
-        <div className="max-md:hidden basis-full md:basis-1/2 lg:basis-1/3 xl:basis-1/4">
+        <div className='max-md:hidden basis-full md:basis-1/2 lg:basis-1/3 xl:basis-1/4'>
           {renderMarquee(secondRow, true)}
         </div>
 
-        <div className="max-lg:hidden basis-full md:basis-1/2 lg:basis-1/3 xl:basis-1/4">
-          {renderMarquee(firstRow)}
+        <div className='max-lg:hidden basis-full md:basis-1/2 lg:basis-1/3 xl:basis-1/4'>
+          {renderMarquee(thirdRow)}
         </div>
-        <div className="max-xl:hidden basis-full md:basis-1/2 lg:basis-1/3 xl:basis-1/4">
-          {renderMarquee(secondRow, true)}
+        <div className='max-xl:hidden basis-full md:basis-1/2 lg:basis-1/3 xl:basis-1/4'>
+          {renderMarquee(fourthRow)}
         </div>
 
         <ReviewDialog
@@ -95,8 +100,8 @@ export function TestimonialMain() {
           onClose={() => setSelectedReview(null)}
         />
 
-        <div className="pointer-events-none absolute inset-x-0 top-0 h-1/5 bg-gradient-to-b from-color1"></div>
-        <div className="pointer-events-none absolute inset-x-0 bottom-0 h-1/5 bg-gradient-to-t from-color1"></div>
+        <div className='pointer-events-none absolute inset-x-0 top-0 h-1/5 bg-gradient-to-b from-color1'></div>
+        <div className='pointer-events-none absolute inset-x-0 bottom-0 h-1/5 bg-gradient-to-t from-color1'></div>
       </div>
     </section>
   );

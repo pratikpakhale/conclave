50d35f50fc0fd1f37f49489c0c550b87bf35666f
Pratik Pakhale <pratikpakhale20@gmail.com>
2024-11-03 14:12:04 +0530

    add: g-tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,25 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-import { Toaster } from "@/components/ui/sonner";
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.css';
+import { GoogleAnalytics } from '@next/third-parties/google';
+
+import { Toaster } from '@/components/ui/sonner';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
 });
 
 export const metadata: Metadata = {
-  title: "IIIT Dharwad Conclave 2024",
-  description: "The official website for HR Conclave 2024, IIIT Dharwad",
-  icons: ["/Brandlogo.png"],
+  title: 'IIIT Dharwad Conclave 2024',
+  description: 'The official website for HR Conclave 2024, IIIT Dharwad',
+  icons: ['/Brandlogo.png'],
 };
 
 export default function RootLayout({
@@ -26,11 +28,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={` font-grotesk antialiased`}>
         {children}
         <Toaster />
       </body>
+      <GoogleAnalytics gaId='G-0519KY429Z' />
     </html>
   );
 }

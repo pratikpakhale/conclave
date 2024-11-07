4cedc61d2c9d9a64fa4f7f3336d34ec9663f9dfc
Pratik Pakhale <pratikpakhale20@gmail.com>
2024-11-07 17:22:21 +0530

    fix: heading fort he timeline in studio

--- sanity/schemaTypes/timeline.ts
+++ sanity/schemaTypes/timeline.ts
@@ -1,46 +1,51 @@
 const timeline = {
-  name: "timeline",
-  title: "Timeline",
-  type: "document",
+  name: 'timeline',
+  title: 'Timeline',
+  type: 'document',
   fields: [
     {
-      title: "Section",
-      name: "section",
-      type: "array",
+      title: 'Event Name',
+      name: 'eventName',
+      type: 'string',
+    },
+    {
+      title: 'Sections',
+      name: 'section',
+      type: 'array',
       of: [
         {
-          type: "object",
+          type: 'object',
           fields: [
             {
-              title: "Heading1",
-              name: "heading1",
-              type: "string",
+              title: 'Heading1',
+              name: 'heading1',
+              type: 'string',
             },
             {
-              title: "Heading2",
-              name: "heading2",
-              type: "string",
+              title: 'Heading2',
+              name: 'heading2',
+              type: 'string',
             },
             {
-              title: "Time",
-              name: "time",
-              type: "string",
+              title: 'Time',
+              name: 'time',
+              type: 'string',
             },
             {
-              title: "Timeline Components",
-              name: "timelineComponents",
-              type: "array",
+              title: 'Timeline Components',
+              name: 'timelineComponents',
+              type: 'array',
               of: [
                 {
-                  type: "object",
+                  type: 'object',
                   fields: [
-                    { name: "time", type: "string", title: "Time" },
-                    { name: "heading", type: "string", title: "Heading" },
+                    { name: 'time', type: 'string', title: 'Time' },
+                    { name: 'heading', type: 'string', title: 'Heading' },
                     {
-                      name: "content",
-                      type: "array",
-                      title: "Content",
-                      of: [{ type: "string" }],
+                      name: 'content',
+                      type: 'array',
+                      title: 'Content',
+                      of: [{ type: 'string' }],
                     },
                   ],
                 },

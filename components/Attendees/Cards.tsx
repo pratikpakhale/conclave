e66638fc58983c0f1bddc9df9aad9d66117f8735
Pratik Pakhale <pratikpakhale20@gmail.com>
2024-11-09 21:51:38 +0530

    fix: poc names styling and linking

--- components/Attendees/Cards.tsx
+++ components/Attendees/Cards.tsx
@@ -1,11 +1,11 @@
-"use client";
-import React, { useState } from "react";
-import { GrAdd } from "react-icons/gr";
-import { FaLinkedin } from "react-icons/fa";
-import { motion, AnimatePresence } from "framer-motion";
-import Image from "next/image";
+'use client';
+import React, { useState } from 'react';
+import { GrAdd } from 'react-icons/gr';
+import { FaLinkedin } from 'react-icons/fa';
+import { motion, AnimatePresence } from 'framer-motion';
+import Image from 'next/image';
 
-import type { Attendee } from "@/app/attendees/page";
+import type { Attendee } from '@/app/attendees/page';
 
 export default function Cards({
   photoUrl: src,
@@ -23,29 +23,29 @@ export default function Cards({
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="w-full max-w-[400px] relative mx-auto">
+    <div className='w-full max-w-[400px] relative mx-auto'>
       <motion.div
         layout
-        className="w-full aspect-square rounded-lg border border-slate-800 bg-white overflow-hidden"
+        className='w-full aspect-square rounded-lg border border-slate-800 bg-white overflow-hidden'
       >
         <motion.div
           layout
-          className="w-full h-full z-[1] relative flex flex-col"
+          className='w-full h-full z-[1] relative flex flex-col'
         >
           {/* Main Card Content */}
-          <div className="flex-1 flex z-[-1] items-center justify-center relative">
+          <div className='flex-1 flex z-[-1] items-center justify-center relative'>
             {src ? (
               <Image
                 width={0}
                 height={0}
-                sizes="100%"
+                sizes='100%'
                 src={src}
                 alt={name}
-                className="w-full h-full object-cover"
+                className='w-full h-full object-cover'
               />
             ) : (
-              <div className="w-24 h-24 rounded-full bg-slate-200 flex items-center justify-center">
-                <span className="text-2xl text-slate-600">
+              <div className='w-24 h-24 rounded-full bg-slate-200 flex items-center justify-center'>
+                <span className='text-2xl text-slate-600'>
                   {name?.charAt(0)}
                 </span>
               </div>
@@ -54,23 +54,23 @@ export default function Cards({
             {/* Add Button */}
             <a
               href={linkedin}
-              target="_blank"
-              className="inline-flex absolute top-4 left-4 items-center space-x-2 text-blue-600 hover:text-blue-700"
+              target='_blank'
+              className='inline-flex absolute top-4 left-4 items-center space-x-2 text-blue-600 hover:text-blue-700'
             >
-              <FaLinkedin className="w-5 h-5" />
+              <FaLinkedin className='w-5 h-5' />
             </a>
 
             {(third_year_poc || fourth_year_poc) && (
               <motion.button
                 whileTap={{ scale: 0.95 }}
                 onClick={() => setIsOpen(!isOpen)}
-                className="absolute top-4 right-4 w-8 h-8 rounded-full bg-slate-100 flex items-center justify-center"
+                className='absolute top-4 right-4 w-8 h-8 rounded-full bg-slate-100 flex items-center justify-center'
               >
                 <motion.div
                   animate={{ rotate: isOpen ? 45 : 0 }}
                   transition={{ duration: 0.2 }}
                 >
-                  <GrAdd className="w-4 h-4" />
+                  <GrAdd className='w-4 h-4' />
                 </motion.div>
               </motion.button>
             )}
@@ -81,24 +81,24 @@ export default function Cards({
             {isOpen && (
               <motion.div
                 initial={{ height: 0, opacity: 0 }}
-                animate={{ height: "auto", opacity: 1 }}
+                animate={{ height: 'auto', opacity: 1 }}
                 exit={{ height: 0, opacity: 0 }}
-                transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
-                className="bg-slate-50/70 backdrop-blur z-[2] w-full bottom-0 absolute"
+                transition={{ type: 'spring', bounce: 0.2, duration: 0.6 }}
+                className='bg-slate-50/70 backdrop-blur z-[2] w-full bottom-0 absolute'
               >
-                <div className="p-6 space-y-4">
+                <div className='p-6 space-y-4'>
                   <motion.div
                     initial={{ y: 20, opacity: 0 }}
                     animate={{ y: 0, opacity: 1 }}
                     exit={{ y: 20, opacity: 0 }}
                     transition={{ delay: 0.1 }}
-                    className="space-y-1"
+                    className='space-y-1'
                   >
-                    <h2 className="font-semibold text-black">Student POC</h2>
+                    <h2 className='font-semibold text-black'>Student POC</h2>
                     <a
                       href={studentLinkedIn2}
-                      target="_blank"
-                      className="font-semibold text-slate-700 text-lg"
+                      target='_blank'
+                      className='font-semibold cursor-pointer text-slate-700 text-lg block z-20 hover:text-blue-600/90'
                     >
                       {third_year_poc}
                     </a>
@@ -108,8 +108,8 @@ export default function Cards({
 
                     <a
                       href={studentLinkedIn1}
-                      target="_blank"
-                      className="font-semibold text-slate-700 leading-4 text-lg"
+                      target='_blank'
+                      className='font-semibold cursor-pointer text-slate-700 leading-4 text-lg block z-20 hover:text-blue-600'
                     >
                       {fourth_year_poc}
                     </a>
@@ -124,11 +124,11 @@ export default function Cards({
         </motion.div>
       </motion.div>
 
-      <div className="space-y-4">
-        <motion.div className="space-y-0">
-          <h3 className="font-semibold text-lg">{name}</h3>
-          <p className="text-slate-600 leading-5">{company}</p>
-          <p className="text-slate-500 leading-5 text-sm">{position}</p>
+      <div className='space-y-4'>
+        <motion.div className='space-y-0'>
+          <h3 className='font-semibold text-lg'>{name}</h3>
+          <p className='text-slate-600 leading-5'>{company}</p>
+          <p className='text-slate-500 leading-5 text-sm'>{position}</p>
         </motion.div>
       </div>
     </div>
